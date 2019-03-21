b9ac812b2455430792f24bcf800c79963cbf2595
Mery1609 <Mery1609@icloud.com>
2019-03-21 06:30:39 -0400

    added 2 new methods

---
+++ src/SearchPage.java
@@ -0,0 +1,15 @@
+
+public class SearchPage {
+
+	public  void searchPage() {
+		
+		System.out.println("This is my commited change");
+	}
+	
+   public  void loginPage() {
+		
+		System.out.println("This is my commited change");
+	}
+	
+
+}
